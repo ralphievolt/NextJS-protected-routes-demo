14fd7954e94e76505ea4a4ecafe09f1a900c8f0e
ralphie <ralpheiligan@gmail.com>
2023-03-18 14:00:36 -0400

    installed clerk

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/nextjs";
+import { useRouter } from "next/router";
+
+const publicPages = ["/"];
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter();
+  const isPublicPage = publicPages.includes(pathname);
+
+  return (
+    <ClerkProvider {...pageProps}>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
